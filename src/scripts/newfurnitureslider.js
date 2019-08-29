da383a8b0c20a68ebcab39567561946a49b4f47e
Maciej Sznurawa <msznurawa@gmail.com>
2019-08-29 10:11:36 +0200

    Set carousel nav as a dots

--- src/scripts/newfurnitureslider.js
+++ src/scripts/newfurnitureslider.js
@@ -4,7 +4,12 @@ const newFurnitureSlider = tns({
   controls: false,
   autoWidth: true,
   items: 1,
-  slideBy: 1,
+  // slideBy: 1,
+  container: '.new-furniture-slider',
+  mouseDrag: true,
+  swipeAngle: false,
+  nav: true,
+  navContainer: '.slider-pagination-dots',
   responsive: {
     576: {
       items: 2,
@@ -18,9 +23,5 @@ const newFurnitureSlider = tns({
       items: 4,
       slideBy: 4
     }
-  },
-  container: '.new-furniture-slider',
-  mouseDrag: true,
-  swipeAngle: false,
-  nav: true
+  }
 });
