af1919d8119c104d76582f8b89b58856dfdf6557
Piotr Wlazeł <pancu1222@gmail.com>
2019-08-29 20:33:42 +0200

    add js logic for star rating in products section and hover over the stars

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
-console.log('hello');
+const stars = document.getElementsByClassName('stars');
+
+for (let i = 0; i < stars.length; ++i) {
+  for (let j = 0; j < stars[i].children.length; ++j) {
+    // console.log(i, j, stars[i].children[j]);
+    stars[i].children[j].addEventListener('click', function () {
+      // console.log('clicked');
+      emptyStar(stars[i].children);
+      fillStar(j, stars[i].children);
+    });
+  }
+}
+
+function fillStar (index, list) {
+  for (let i = 0; i < index + 1; i++) {
+    list[i].classList.add('full');
+  }
+}
+
+function emptyStar (list) {
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.remove('full');
+  }
+}
