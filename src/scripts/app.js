26b22ef99d98d8a28be198298fc1216ce1aa18c3
Piotr Wlazeł <pancu1222@gmail.com>
2019-08-29 20:46:47 +0200

    removed console.logs from app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,9 +2,7 @@ const stars = document.getElementsByClassName('stars');
 
 for (let i = 0; i < stars.length; ++i) {
   for (let j = 0; j < stars[i].children.length; ++j) {
-    // console.log(i, j, stars[i].children[j]);
     stars[i].children[j].addEventListener('click', function () {
-      // console.log('clicked');
       emptyStar(stars[i].children);
       fillStar(j, stars[i].children);
     });
