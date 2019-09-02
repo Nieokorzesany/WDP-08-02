6d0ab045216f6a50340493cde9bdd5a8fe92a7d1
Maciej Sznurawa <msznurawa@gmail.com>
2019-09-02 13:19:41 +0200

    Task finished

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,25 @@
-console.log('hello');
+import { tns } from './../../node_modules/tiny-slider/src/tiny-slider';
+
+// Section New Furniture Slider
+// eslint-disable-next-line no-unused-vars
+const newFurnitureSlider = tns({
+  container: '.new-furniture-slider',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  mouseDrag: true,
+  swipeAngle: 20,
+  startIndex: 1,
+  navContainer: '.slider-pagination-dots',
+  responsive: {
+    576: {
+      items: 2
+    },
+    992: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
