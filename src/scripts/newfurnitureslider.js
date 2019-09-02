919cb32dfcda9feafca31c48bd8924c4dfa399bc
Maciej Sznurawa <msznurawa@gmail.com>
2019-09-02 08:18:49 +0200

    Fix dots displaying problem on mobiles

--- src/scripts/newfurnitureslider.js
+++ src/scripts/newfurnitureslider.js
@@ -2,13 +2,13 @@
 import { tns } from './../../node_modules/tiny-slider/src/tiny-slider';
 // eslint-disable-next-line no-unused-vars
 const newFurnitureSlider = tns({
-  controls: false,
-  autoWidth: true,
-  slideBy: 'page',
   container: '.new-furniture-slider',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
   mouseDrag: true,
   swipeAngle: 20,
-  nav: false,
+  navContainer: '.slider-pagination-dots',
   responsive: {
     576: {
       items: 2
@@ -23,52 +23,52 @@ const newFurnitureSlider = tns({
 });
 
 const DOTS = document.getElementsByClassName('pagination-dot-li');
-const VIEWPORT_WIDTH = document.documentElement.clientWidth;
+// const VIEWPORT_WIDTH = document.documentElement.clientWidth;
 
-// funkcja wyśiwetlająca odpowiednie kropki w zależności szerokości okna
-function showDot () {
-  for (let i = 0; i < DOTS.length; i++) {
-    DOTS[i].classList.remove('show-dot');
-    DOTS[0].classList.add('show-dot', 'active');
-    if (VIEWPORT_WIDTH >= 1200) {
-      let numberofSlides = 4;
-      if ((i + 1) % (numberofSlides + 1) === 0) {
-        DOTS[i].classList.add('show-dot');
-      }
-    } else if (VIEWPORT_WIDTH >= 992 && VIEWPORT_WIDTH < 1200) {
-      let numberofSlides = 3;
-      if (
-        (i + 1) % (numberofSlides + 1) === 0 ||
-        (i + 4) % (numberofSlides + 4) === 0
-      ) {
-        DOTS[i].classList.add('show-dot');
-      }
-    } else if (VIEWPORT_WIDTH >= 576 && VIEWPORT_WIDTH < 992) {
-      let numberofSlides = 2;
-      if (
-        (i + 1) % (numberofSlides + 1) === 0 ||
-        (i + 3) % (numberofSlides + 3) === 0 ||
-        (i + 5) % (numberofSlides + 5) === 0
-      ) {
-        DOTS[i].classList.add('show-dot');
-      }
-    } else {
-      DOTS[i].classList.add('show-dot');
-    }
-  }
-}
-showDot();
+// // funkcja wyśiwetlająca odpowiednie kropki w zależności szerokości okna
+// function showDot () {
+//   for (let i = 0; i < DOTS.length; i++) {
+//     DOTS[i].classList.remove('show-dot');
+//     DOTS[0].classList.add('show-dot', 'active');
+//     if (VIEWPORT_WIDTH >= 1200) {
+//       let numberofSlides = 4;
+//       if ((i + 1) % (numberofSlides + 1) === 0) {
+//         DOTS[i].classList.add('show-dot');
+//       }
+//     } else if (VIEWPORT_WIDTH >= 992 && VIEWPORT_WIDTH < 1200) {
+//       let numberofSlides = 3;
+//       if (
+//         (i + 1) % (numberofSlides + 1) === 0 ||
+//         (i + 4) % (numberofSlides + 4) === 0
+//       ) {
+//         DOTS[i].classList.add('show-dot');
+//       }
+//     } else if (VIEWPORT_WIDTH >= 576 && VIEWPORT_WIDTH < 992) {
+//       let numberofSlides = 2;
+//       if (
+//         (i + 1) % (numberofSlides + 1) === 0 ||
+//         (i + 3) % (numberofSlides + 3) === 0 ||
+//         (i + 5) % (numberofSlides + 5) === 0
+//       ) {
+//         DOTS[i].classList.add('show-dot');
+//       }
+//     } else {
+//       DOTS[i].classList.add('show-dot');
+//     }
+//   }
+// }
+// showDot();
 
-// wywołanie funkcji showDot po zmianie szerokości okna
-let resizeId;
-window.addEventListener('resize', function () {
-  clearTimeout(resizeId);
-  resizeId = setTimeout(doneResizing, 500);
-});
+// // wywołanie funkcji showDot po zmianie szerokości okna
+// let resizeId;
+// window.addEventListener('resize', function () {
+//   clearTimeout(resizeId);
+//   resizeId = setTimeout(doneResizing, 500);
+// });
 
-function doneResizing () {
-  showDot();
-}
+// function doneResizing () {
+//   showDot();
+// }
 
 //  obsługa klików
 //  funkcja ogólna
@@ -78,17 +78,17 @@ function click (target, callback) {
     return callback(e);
   });
 }
-// zmiana slajdów
-for (let i = 0; i < DOTS.length; i++) {
-  let k = i + 1;
-  DOTS[i].onclick = function () {
-    newFurnitureSlider.goTo(k);
-  };
-}
+// // zmiana slajdów
+// for (let i = 0; i < DOTS.length; i++) {
+//   let k = i + 1;
+//   DOTS[i].onclick = function () {
+//     newFurnitureSlider.goTo(k);
+//   };
+// }
 // zmiana koloru kropki
 click('.slider-pagination-dots', function (e) {
   let target = e.target;
-  if (target.parentElement.classList.value === 'pagination-dot-li show-dot') {
+  if (target.parentElement.classList.value === 'pagination-dot-li tns-nav-active') {
     for (var j = 0; j < DOTS.length; j++) {
       DOTS[j].classList.remove('active');
     }
