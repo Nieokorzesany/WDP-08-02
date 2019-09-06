0128318244e7533e012f4d5664c798f00060e789
Maciej Sznurawa <msznurawa@gmail.com>
2019-09-06 18:38:38 +0200

    Add slider to each product category, add scripts that shuuld be improved

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -103,26 +103,124 @@ function emptyStar (list) {
   }
 }
 
-// eslint-disable-next-line no-unused-vars
+// Featured Gallery Slider
 const gallerySliderFeatured = tns({
   container: '#slider-featured',
+  mode: 'gallery',
+  speed: 800,
   items: 1,
   controls: false,
-  nav: false,
-  mouseDrag: true,
-  swipeAngle: 20
+  nav: false
 });
 
 // eslint-disable-next-line no-unused-vars
-// const gallerySliderFeaturedThumbnails = tns({
-//   container: '#featured-control-thumbnails',
-//   items: 6,
-//   slideBy: 'page',
-//   prevButton: '.control-arrow-prev',
-//   nextButton: '.control-arrow-next',
-//   nav: false
-//   // navContainer: '.controls-thumbnails',
-//   // navAsThumbnails: true,
-//   // mouseDrag: true,
-//   // swipeAngle: 20
-// });
+const gallerySliderTopseller = tns({
+  container: '#slider-topseller',
+  mode: 'gallery',
+  speed: 800,
+  items: 1,
+  controls: false,
+  nav: false
+});
+
+// eslint-disable-next-line no-unused-vars
+const gallerySliderSaleoff = tns({
+  container: '#slider-saleoff',
+  mode: 'gallery',
+  speed: 800,
+  items: 1,
+  controls: false,
+  nav: false
+});
+
+// eslint-disable-next-line no-unused-vars
+const gallerySliderToprated = tns({
+  container: '#slider-toprated',
+  mode: 'gallery',
+  speed: 800,
+  items: 1,
+  controls: false,
+  nav: false
+});
+
+const thumbnails = document.getElementsByClassName('controls-thumbnails');
+console.log(thumbnails[0].id);
+
+const thumbnailsArray = [];
+for (let i = 0; i < thumbnails.length; i++) {
+  thumbnailsArray.push("'#" + thumbnails[i].id + " > li'");
+}
+console.log(thumbnailsArray.length);
+console.log(thumbnailsArray[0]);
+
+// for (let j = 0; j < thumbnailsArray.length; j++) {
+//   for (let i = 0; i < thumbnailsArray[i].children.length; i++) {
+//     thumbnailsArray[i].children.addEventListener('click', function (e) {
+//       e.preventDefault();
+//       slideOverlay();
+//       gallerySliderFeatured.goTo(i);
+//       e.target.parentElement.classList.remove('overlay');
+//     });
+//   }
+// }
+
+const featuredThumbnails = document.querySelectorAll(
+  '#featured-controls-thumbnails > li'
+);
+// przewijam slajdy kliknięciem w miniaturki i toglujemy overlaya
+for (let i = 0; i < featuredThumbnails.length; i++) {
+  featuredThumbnails[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    slideOverlay();
+    gallerySliderFeatured.goTo(i);
+    e.target.parentElement.classList.remove('overlay');
+  });
+}
+
+// obsługa strzałek
+const featuredControlArrows = document.getElementsByClassName('featured-control-arrow');
+for (let i = 0; i < featuredControlArrows.length; i++) {
+  featuredControlArrows[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    slideThumbnails();
+  });
+}
+
+// ogarnąc to z name jako argumentem
+function slideThumbnails (name) {
+  for (let i = 0; i < featuredThumbnails.length; i++) {
+    featuredThumbnails[i].classList.toggle('hide');
+  }
+}
+
+function slideOverlay (name) {
+  for (let i = 0; i < featuredThumbnails.length; i++) {
+    featuredThumbnails[i].classList.add('overlay');
+  }
+}
+
+const productCategories = document.getElementsByClassName('product-category-name');
+const furnitureGallerySliders = document.getElementsByClassName(
+  'furniture-gallery-slider'
+);
+const furnitureGalleryControls = document.getElementsByClassName(
+  'furniture-gallery-controls'
+);
+for (let i = 0; i < productCategories.length; i++) {
+  productCategories[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    showFurnitureSlider();
+    showFurnitureGalleryControls();
+  });
+}
+
+function showFurnitureSlider () {
+  for (let i = 0; i < furnitureGallerySliders.length; i++) {
+    furnitureGallerySliders[i].classList.toggle('hide');
+  }
+}
+function showFurnitureGalleryControls () {
+  for (let i = 0; i < furnitureGalleryControls.length; i++) {
+    furnitureGalleryControls[i].classList.toggle('hide');
+  }
+}
